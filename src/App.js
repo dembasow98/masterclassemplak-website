a995c980a53936b9eb94149ce5afa7cdd5a41ea0
dembasow98 <dastech1998@gmail.com>
2023-02-16 02:19:45 +0300

    Structured the application folder.

--- src/App.js
+++ src/App.js
@@ -2,10 +2,9 @@
 
 function App() {
   return (
+    
     <div className="flex w-full h-screen bg-gray-900 dark:text-white">
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+     
     </div>
   );
 }
