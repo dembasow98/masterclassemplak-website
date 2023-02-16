34a977aa8b5d09a903d24b46eaa21106247f7fc2
dembasow98 <dastech1998@gmail.com>
2023-02-16 23:01:18 +0300

    Done with Topbar UI.

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -2,3 +2,6 @@
 export {default as Topbar} from './topbar/Topbar';
 export {default as Navbar} from './navbar/Navbar';
 export {default as LandingSlider} from './slider/LandingSlider';
+
+
+export {default as Language} from './dropdown/Language';
--- src/components/home/topbar/Topbar.jsx
+++ src/components/home/topbar/Topbar.jsx
@@ -1,43 +1,96 @@
 import React from 'react'
-import {BsFillCaretLeftSquareFill,BsFillCaretRightSquareFill } from "react-icons/bs";
-import { FaHandMiddleFinger } from "react-icons/fa";
+import {MdOutlineManageSearch} from "react-icons/md";
+import {FaEthereum} from "react-icons/fa";
+import {SiBitcoin, SiLitecoin} from "react-icons/si";
+import {TiGroup} from "react-icons/ti";
 
 
 const Topbar = () => {
   return (
-    <div className='flex w-full h-24 sm:h-22 md:h-16 lg:h-14 bg-white text-black dark:text-white dark:bg-gray-900'>
-      <div className='flex flex-col md:flex-row w-full bg-green-400 items-center justify-center mx-8 md:mx-6 lg:mx-4'>
-        <div className='flex items-center justify-between w-full px-4 mx-auto bg-yellow-300'>
-          <div className='right-part '>
-          <BsFillCaretLeftSquareFill className='text-3xl ' />
-          </div>
-          <div className='middle-part'>
-            <FaHandMiddleFinger className = "text-3xl"/>
-          </div>
-          <div className='left-part'>
-            <BsFillCaretRightSquareFill className='text-3xl' />
-          </div>
-        </div>
+    <div className='flex w-full h-24 sm:h-22 md:h-16 lg:h-14 bg-white text-black dark:text-white dark:bg-gray-900  border-b dark:border-gray-300'>
+      <div className='flex flex-col md:flex-row w-full items-center justify-center md:mx-6 lg:mx-4'>
         
-        <div className='flex flex-row items-center justify-between w-full px-4 mx-auto bg-pink-300'>
-          <ul className='flex flex-row items-center justify-between w-full px-4 mx-auto'> 
-              <li>
-                <a href="https://www.facebook.com/">₺</a>
-              </li>
-              <li>
-                <a href="https://www.instagram.com/">$</a>
-              </li>
-              <li>
-                <a href="https://www.twitter.com/">€</a>
-              </li>
-              <li>
-                <a href="https://www.linkedin.com/">£</a>
-              </li>
-            </ul>
+        <div className='TOP-PART md:order-2 flex items-center justify-between md:justify-center w-full px-4 '>
+          <div className='LEFT-PART flex flex-row p-2 gap-3'>
+            
+            <div className='CURENCY items-center justify-between w-full px-2 py-1 rounded-md border-2 border-red-700'>
+              <ul className='flex gap-3 flex-row' > 
+                <li>
+                  <a href="https://www.facebook.com/">₺</a>
+                </li> 
+                <li>
+                  <a href="https://www.instagram.com/">$</a>
+                </li>
+                <li>
+                  <a href="https://www.twitter.com/">€</a>
+                </li>
+                <li>
+                  <a href="https://www.linkedin.com/">£</a>
+                </li>
+              </ul>
+            </div>
+            
+            <div className='LANGUAGES flex w-full bg-yellow-800 px-3 md:px-4 rounded-md justify-center items-center'>
+              <a href="https://www.facebook.com/">Language</a>
+            </div>
+          </div>
+
+          <div className='BUY-ONLINE flex w-full  justify-end place-items-center'>
+            <button type="button" class="w-fit focus:outline-none text-white bg-red-800 hover:bg-red-900 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">
+              BUY ONLINE
+            </button>
+          </div>
+          
         </div>
+
+        <div className='BOTTOM-PART md:order-1 flex flex-row items-center justify-center gap-3 w-full px-4 mx-auto '>
+
+          <div className='USER flex flex-row  p-2 gap-3'>
+            <div  className='flex w-full bg-yellow-800 px-3 md:py-2 rounded-md justify-center items-center'>
+              <a className='flex w-full items-center gap-2' href="https://www.facebook.com/">
+                <TiGroup className='text-xl md:text-2xl'/>
+                <span className='text-sm hidden md:inline-flex'>Login/Register</span>
+              </a>
+            </div>
+          </div>
+
+          <div className='PAYMENT'>
+            <ul className='flex flex-row gap-3'>
+              <li>
+                <a href="https://www.instagram.com/">
+                  <SiBitcoin className='text-xl md:text-2xl'/>
+                </a>
+              </li>
+              <li>
+                <a href="https://www.facebook.com/">
+                  <FaEthereum className='text-xl md:text-2xl'/>
+                </a>
+              </li>
+              
+              <li>
+                <a href="https://www.twitter.com/">
+                  <SiLitecoin className='text-xl md:text-2xl'/>
+                </a>
+              </li>
+              <span className='text-sm md:text-lg '>Accepted here</span>
+            </ul>
+            
+          </div>
+          
+          <div className='SEARCH flex flex-row p-2 gap-3'>
+            <div className='flex w-full px-3 rounded-md justify-center items-center'>
+              <a className='flex w-full items-center gap-2' href="https://www.facebook.com/">
+                <MdOutlineManageSearch className='text-xl md:text-2xl'/>
+                <span className='text-sm md:text-lg hidden md:inline-flex'>Search</span>
+              </a>
+            </div>
+          </div>
+
+        </div>
+
       </div>
     </div>
   )
 }
 
-export default Topbar
+export default Topbar;
