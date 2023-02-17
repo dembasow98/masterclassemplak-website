66ca5bc8ae472dc8a60b4dcc421042876a9abf0e
dembasow98 <dastech1998@gmail.com>
2023-02-17 21:34:32 +0300

    Done with the Navbar UI.

--- src/App.js
+++ src/App.js
@@ -4,13 +4,12 @@ import Navigation from './containers/Navigation'
 import {Home, Blogs, Contact, About, Properties, News} from './pages'
 import { Topbar, Navbar, LandingSlider } from './components/home';
 import Footer from './pages/Footer';
-import Test from './containers/Test';
 
 function App() {
   return (
     <Router>
       <Topbar/>
-      <Test />
+      <Navigation />
       <Routes>
         <Route path="/" element={<Home />} >
           <Route path="/" element={<Topbar />} />
--- src/containers/Navigation.js
+++ src/containers/Navigation.js
@@ -1,13 +1,14 @@
-import React, { useState, useEffect } from 'react'
-
+import React, { useState, useEffect } from "react";
+import { Transition } from "@headlessui/react";
 import { Link } from 'react-router-dom'
 
-import logo_dark from '../assets/images/logos/logo-dark.png'
 import logo_light from '../assets/images/logos/logo-light.png'
+import logo_dark from '../assets/images/logos/logo-dark.png'
 
 
-const Navigation = () => {
-  //topbar h-24
+function Navigation() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   const [navbarFixed, setNavbarFixed] = useState(false);
   useEffect(() => {
     const handleScroll = () => {
@@ -22,78 +23,184 @@ const Navigation = () => {
       window.removeEventListener('scroll', handleScroll);
     };
   }, []);
-  //Menu
-  const [menuOpen, setMenuOpen] = useState(false);
 
   return (
-    <nav 
-    className={`${navbarFixed ? 'fixed top-0 left-0 right-0 bg-[#003032] shadow-md z-50 ' : 'relative'
-    } flex w-full h-20 bg-[#003032]  text-white items-center  px-4 py-3`}>
-        <div className='MENU flex items-center md:justify-between w-full h-fit'>
-          <div className="LOGO flex flex-col items-center w-48">
-            <img src={logo_light} alt="logo" className="w-10" />
-            <span className="text-sm md:text-base font-bold rounded-md ml-2 ">Masterclass <span className='bg-yellow-900'>Emlak</span></span>
-          </div>
-          <div className='md:hidden flex w-full justify-end'>
-            <button
-              data-collapse-toggle="navbar-sticky" type="button"
-              aria-controls="navbar-sticky" aria-expanded="false" aria-label="Toggle navigation"
-              className="block items-center px-2 py-1 border rounded text-gray-700 dark:text-yellow-900 border-yellow-800 hover:text-yellow-600 hover:border-yellow-600"
-              onClick={() => setMenuOpen(!menuOpen)}
-            >
-              <svg
-                className={`w-8 ${menuOpen ? 'hidden' : 'block'}`}
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
+    <div>
+      <nav className={`${navbarFixed ?"fixed top-0 left-0 right-0 shadow-md z-50":"relative"} bg-[#043334] `}>
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+          <div className="flex items-center justify-between h-16">
+            <div className="flex items-center md:justify-between md:w-full">
+              <div className="flex-shrink-0">
+                <img src={logo_light} alt="logo" className="w-10" />
+                <span className="text-sm text-white dark:text-yellow-500 md:text-base font-bold rounded-md ml-2 ">Masterclass <span className='bg-yellow-900 dark:text-gray-200'>Emlak</span></span>
+              </div>
+              <div className="hidden md:block">
+                <div className="ml-10 flex items-baseline space-x-4  md:flex md:flex-row  md:gap-6  lg:gap-10 xl:gap-12 2xl:gap-16">
+                  <Link
+                    onClick={() => setMenuOpen(false)}
+                    to="/"
+                    className=" text-gray-300 dark:hover:bg-[#014649] hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                  >
+                    Home
+                  </Link>
 
-                  fillRule="evenodd"
-                  d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                  clipRule="evenodd"
-                />
-              </svg>
-              <svg
-                className={`w-8 ${menuOpen ? 'block' : 'hidden'}`}
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
+                  <Link
+                    to="/about" onClick={() => setMenuOpen(false)}
+                    className="text-gray-300 dark:hover:bg-[#014649] hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                  >
+                    About
+                  </Link>
+
+                  <Link
+                    to="/properties" onClick={() => setMenuOpen(false)}
+                    className="text-gray-300 dark:hover:bg-[#014649] hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                  >
+                    Properties
+                  </Link>
+
+                  <Link
+                    to="/news" onClick={() => setMenuOpen(false)}
+                    className="text-gray-300 dark:hover:bg-[#014649] hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                  >
+                    News
+                  </Link>
+
+                  <Link
+                    to="/blogs" onClick={() => setMenuOpen(false)}
+                    className="text-gray-300 dark:hover:bg-[#014649] hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                  >
+                    Blogs
+                  </Link>
+                  
+                  <Link
+                    to="/contact" onClick={() => setMenuOpen(false)}
+                    className="text-gray-300 dark:hover:bg-[#014649] hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                  >
+                    Contact
+                  </Link>
+                </div>
+              </div>
+            </div>
+            <div className="-mr-2 flex md:hidden">
+              <button
+                onClick={() => setMenuOpen(!menuOpen)}
+                data-collapse-toggle="navbar-sticky" type="button"
+                aria-controls="navbar-sticky" aria-expanded="false" aria-label="Toggle navigation"
+                className="block items-center px-2 py-1 border rounded text-gray-700 dark:text-yellow-900 border-yellow-800 hover:text-yellow-600 hover:border-yellow-600"
               >
-                <path
-                  fillRule="evenodd"
-                  d="M14.95 5.364a1 1 0 10-1.414-1.414L5.636 13.95a1 1 0 001.414 1.414L14.95 5.364zM6.05 5.364a1 1 0 011.414-1.414L14.364 13.95a1 1 0 00-1.414 1.414-1.414L6.05 5.364z"
-                  clipRule="evenodd"
-                />
-              </svg>
-            </button>
-          </div>
-          
-          <div className={`MENU-ITEMS w-full z-20 md:z-0 md:inline-flex items-center md:w-auto ${menuOpen ? 'block' : 'hidden'}`}>
-            <ul className='flex flex-col p-4 mt-4 border-gray-100 bg-gray-50  md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-[#003032] md:dark:bg-[#003032] md:flex md:flex-row  md:gap-6  lg:gap-10 xl:gap-12 2xl:gap-16'>
-              <li className='block  items-center'>
-                <Link to="/" onClick={() => setMenuOpen(false)} >Home</Link>
-              </li>
-              <li className='block items-center'>
-                <Link to="/about" onClick={() => setMenuOpen(false)}>About</Link>
-              </li>
-              <li className='block items-center'>
-                <Link to="/properties" onClick={() => setMenuOpen(false)}>Properties</Link>
-              </li>
-              <li className='block items-center'>
-                <Link to="/news" onClick={() => setMenuOpen(false)}>News</Link>
-              </li>
-              <li className='block items-center'>
-                <Link to="/blogs" onClick={() => setMenuOpen(false)}>Blogs</Link>
-              </li>
-              <li className='block items-center'>
-                <Link to="/contact" onClick={() => setMenuOpen(false)}>Contact</Link>
-              </li>
-            </ul>
-            
+                <span className="sr-only">Open main menu</span>
+                {!menuOpen ? (
+                  <svg
+                    className="block h-6 w-6"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    aria-hidden="true"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M4 6h16M4 12h16M4 18h16"
+                    />
+                  </svg>
+                ) : (
+                  <svg
+                    className="block h-6 w-6"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    aria-hidden="true"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M6 18L18 6M6 6l12 12"
+                    />
+                  </svg>
+                )}
+              </button>
+            </div>
           </div>
         </div>
-    </nav>
-  )
+
+        <Transition
+          show={menuOpen}
+          enter="transition ease-out duration-100 transform"
+          enterFrom="opacity-0 scale-95"
+          enterTo="opacity-100 scale-100"
+          leave="transition ease-in duration-75 transform"
+          leaveFrom="opacity-100 scale-100"
+          leaveTo="opacity-0 scale-95"
+        >
+          {(ref) => (
+            <div className="md:hidden" id="mobile-menu">
+              <div ref={ref} className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
+                <Link
+                  to="/" onClick={() => setMenuOpen(false)} 
+                  className="text-gray-300 dark:hover:bg-[#014649] hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                >
+                  Home
+                </Link>
+
+                <Link
+                  to="/about" onClick={() => setMenuOpen(false)}
+                  className="text-gray-300 dark:hover:bg-[#014649] hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                >
+                  About
+                </Link>
+
+                <Link
+                   to="/properties" onClick={() => setMenuOpen(false)}
+                  className="text-gray-300 dark:hover:bg-[#014649] hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                >
+                  Properties
+                </Link>
+
+                <Link
+                  to = "/news" onClick={() => setMenuOpen(false)}
+                  className="text-gray-300 dark:hover:bg-[#014649] hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                >
+                  News
+                </Link>
+
+                <Link
+                  to="/blogs" onClick={() => setMenuOpen(false)}
+                  className="text-gray-300 dark:hover:bg-[#014649] hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                >
+                  Blogs
+                </Link>
+                <Link
+                  to="/contact" onClick={() => setMenuOpen(false)}
+                  className="text-gray-300 dark:hover:bg-[#014649] hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                >
+                  Contact
+                </Link>
+              </div>
+            </div>
+          )}
+        </Transition>
+      </nav>
+
+      <header className="bg-white shadow">
+        <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+          <h1 className="text-3xl font-bold text-gray-900">Dashboard</h1>
+        </div>
+      </header>
+      <main>
+        <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+          {/* <!-- Replace with your content --> */}
+          <div className="px-4 py-6 sm:px-0">
+            <div className="border-4 border-dashed border-gray-200 rounded-lg h-96"></div>
+          </div>
+          {/* <!-- /End replace --> */}
+        </div>
+      </main>
+    </div>
+  );
 }
-export default Navigation
+
+export default Navigation;
