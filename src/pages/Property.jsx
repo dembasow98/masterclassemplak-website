fa80a7203fb9f351c832a7cbd96c54a694de949c
dembasow98 <dastech1998@gmail.com>
2023-02-28 07:36:38 +0300

    Added the sticky feature for the Enquire component.

--- src/pages/Property.jsx
+++ src/pages/Property.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {useLocation } from 'react-router-dom';
 
 import { Head, Gallery, Details } from './../components/properties/property';
@@ -14,6 +14,18 @@ const Property = () => {
   //Get property details from the state
   const { title, price, reference, createdAt, updatedAt, gallery, description, overview, features, benefits, details } = state;
   //console.log("From details:"+features);
+
+  //this is to PREVENT the Enquire component from scrolling to the top of the page
+
+  //This happens starting from the middle breakpoint
+  const [scroll, setScroll] = useState(false);
+
+  useEffect(() => {
+    if (window.innerWidth > 768) {
+      setScroll(true);
+    }
+  }, [scroll]);
+
   return (
     <main className="bg-gray-100 mx-auto px-auto dark:bg-gray-900">
       <Route component="Properties" breadcrumbs={title} />
@@ -36,7 +48,10 @@ const Property = () => {
             details={details}
           />
         </div>
-        <div className="w-full flex flex-col items-center md:mt-4 md:w-1/3 justify-center">
+        <div 
+          className="w-full flex flex-col items-center md:mt-4 md:w-1/3 justify-center"
+          style={{ position: scroll ? "sticky" : "relative", top: scroll ? "6rem" : "0" }}
+        >
           <Enquire />
         </div>
       </section>
