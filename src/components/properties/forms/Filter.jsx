ebfa33879d6c7a38bc6a38188b0d61d2818deccf
dembasow98 <dastech1998@gmail.com>
2023-03-02 08:04:50 +0300

    Property filter form UI implemented.

---
+++ src/components/properties/forms/Filter.jsx
@@ -0,0 +1,178 @@
+import React, {useState} from "react";
+import {HiSelector} from "react-icons/hi";
+
+
+const Filter = () => {
+
+
+  const [filterData, setFilterData] = useState({
+    referenceId: "",
+    city: "",
+    type: "",
+    minPrice: "",
+    maxPrice: "",
+    bedrooms: "",
+  });
+
+  //Handle the form input changes
+  const handleFilterChange = (e) => {
+    const {name, value} = e.target;
+    setFilterData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+
+  //Get the data after the form is submitted
+  const handleFilterSubmit = (e) => {
+    e.preventDefault();
+    console.log(filterData);
+
+
+    //TODO: Send the data to the backend
+
+
+    //TODO: Reset the form
+    setFilterData({
+      referenceId: "",
+      city: "",
+      type: "",
+      minPrice: "",
+      maxPrice: "",
+      bedrooms: "",
+    });
+  };
+
+  return (
+    <section className="w-full mt-8 flex  justify-center items-center">
+      <div className = "w-full flex justify-center items-center">
+        <form onChange={handleFilterChange} className="w-full flex  flex-col bg-white dark:bg-gray-800 px-6 pt-4 pb-2 rounded-lg shadow-md filter-shadow-inset">
+          <div className = "w-full flex justify-center py-2 items-center">
+            <span className="w-full text-center text-2xl py-2  font-bold text-gray-700 dark:text-gray-200">Find Your Appropriate Property</span>
+          </div>
+          <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-3">
+            <div className="mb-4">
+              <div className="relative">
+                <input placeholder="Reference ID" type="text" name="referenceId" value={filterData?.referenceId} className="block appearance-none w-full bg-white dark:text-gray-200 dark:bg-gray-900 border-gray-400 hover:border-gray-500 px-2 py-2 rounded shadow leading-tight focus:outline-none focus:border-gray-400 focus:shadow-outline" id="referenceId" />
+              </div>
+            </div>
+            <div className="mb-4">
+                <div className="relative">
+                    <select 
+                      value={filterData?.city}
+                      name="city"
+                      className="block appearance-none w-full bg-white dark:text-gray-200 dark:bg-gray-900 border-gray-400 hover:border-gray-500 px-2 py-2 rounded shadow leading-tight focus:outline-none focus:shadow-outline" id="heating">
+                        <option>City</option>
+                        <option>All</option>
+                        <option>Ankara</option>
+                        <option>Istanbul</option>
+                        <option>Oil</option>
+                        <option>Wood</option>
+                        <option>Other</option>
+                    </select>
+                    <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                        <HiSelector className="dark:text-gray-100"/>
+                    </div>
+                </div>
+              </div>
+              
+
+              <div className="mb-4">
+                  <div className="relative">
+                      <select 
+                        value={filterData?.type}
+                        name="type"
+                        className="block appearance-none w-full bg-white dark:text-gray-200 dark:bg-gray-900 border-gray-400 hover:border-gray-500 px-2 py-2 rounded shadow leading-tight focus:outline-none focus:shadow-outline" id="heating">
+                          <option>Type</option>
+                          <option>All</option>
+                          <option>Gas</option>
+                          <option>Electric</option>
+                          <option>Oil</option>
+                          <option>Wood</option>
+                          <option>Other</option>
+                      </select>
+                      <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                      <HiSelector className="dark:text-gray-100"/>
+                      </div>
+                  </div>
+              </div>
+
+              <div className="mb-4">
+                  <div className="relative">
+                      <select 
+                        value={filterData?.bedrooms}
+                        name="bedrooms"
+                        className="block appearance-none w-full bg-white dark:text-gray-200 dark:bg-gray-900 border-gray-400 hover:border-gray-500 px-2 py-2 rounded shadow leading-tight focus:outline-none focus:shadow-outline" id="heating">
+                          <option>Minimum Price</option> 
+                          <option>Gas</option>
+                          <option>Electric</option>
+                          <option>Oil</option>
+                          <option>Wood</option>
+                          <option>Other</option>
+                      </select>
+                      <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                        <HiSelector className="dark:text-gray-100"/>
+                      </div>
+                  </div>
+              </div>
+              <div className="mb-4">
+                  <div className="relative">
+                      <select 
+                        value={filterData?.bedrooms}
+                        name="bedrooms"
+                        className="block appearance-none w-full bg-white dark:text-gray-200 dark:bg-gray-900 border-gray-400 hover:border-gray-500 px-2 py-2 rounded shadow leading-tight focus:outline-none focus:shadow-outline" id="heating">
+                          <option>Maximum Price</option>
+                          <option>Gas</option>
+                          <option>Electric</option>
+                          <option>Oil</option>
+                          <option>Wood</option>
+                          <option>Other</option>
+                      </select>
+                      <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                        <HiSelector className="dark:text-gray-100"/>
+                      </div>
+                  </div>
+              </div>
+
+              
+              <div className="mb-4">
+                  <div className="relative">
+                      <select 
+                        value={filterData?.bedrooms}
+                        name="bedrooms"
+                        className="block appearance-none w-full bg-white dark:text-gray-200 dark:bg-gray-900 border-gray-400 hover:border-gray-500 px-2 py-2 rounded shadow leading-tight focus:outline-none focus:shadow-outline" id="heating">
+                          <option> Bedrooms</option>
+                          <option>All</option>
+                          <option>1</option>
+                          <option>2</option>
+                          <option>3</option>
+                          <option>4</option>
+                          <option>5</option>
+                          <option>6</option>
+                          <option>7</option>
+                          <option>8</option>
+                          <option>9</option>
+                          <option>10</option>
+                          <option>11+</option>
+                      </select>
+                      <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                        <HiSelector className="dark:text-gray-100"/>
+                      </div>
+                  </div>
+                  <div className = "w-full flex justify-center items-center sm:w-full  pt-4">
+                    <button onClick={handleFilterSubmit} className="w-full bg-blue-500 mx-4 dark:bg-red-600  hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
+                      Search
+                    </button>
+                  </div>
+              </div>
+          </div>
+        </form>
+      </div>
+    </section>
+
+            
+  )
+}
+
+export default Filter
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import { Banner } from '../components/home'
-//import Languages from '../components/common/dropdown/Languages'
+
+import {Filter } from '../components/properties/forms'
 
 const Home = () => {
+
+
   return (
     <main className="bg-gray-100 mx-auto px-8 py-8 dark:bg-gray-900">
       <Banner />
+      <Filter />
+      
     </main>
   )
 }
