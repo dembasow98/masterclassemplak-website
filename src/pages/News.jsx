837d5f6a6f61ea40a28bad4f1aaf31595489546d
dembasow98 <dastech1998@gmail.com>
2023-05-20 12:53:58 +0300

    Changed the images sources.

--- src/pages/News.jsx
+++ src/pages/News.jsx
@@ -5,7 +5,7 @@ import underdev from "../assets/images/error/underdev.gif";
 const News = () => {
     return (
         <div class="w-full">
-            <div class="w-full my-48 lg:my-0">
+            <div class="w-full my-48 lg:my-10">
                 <img src={underdev} alt="under construction" class="w-full object-cover" />
             </div>
             <Design />
